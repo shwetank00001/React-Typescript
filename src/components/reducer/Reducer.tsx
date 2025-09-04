765f53188cd92827a56900c3a818a4872d303023
Shwetank <mshwetank@gmail.com>
2025-09-04 15:09:10 +0530

    Working and debugging with TS

--- src/components/reducer/Reducer.tsx
+++ src/components/reducer/Reducer.tsx
@@ -1,17 +1,35 @@
-import { useReducer } from 'react'
+import { useReducer, useState } from 'react'
 
 const Reducer = () => {
 
     function reducer(state, action){
-
     }
 
     const defaultState = {
-
+        protein: 0
     }
+
     const [state, dispatch] = useReducer(Reducer, defaultState);
+    const [proteinInput, setInput] = useState('');
+
+    const addProtein = (e:React.FormEvent<HTMLFormElement>) => {
+        e.preventDefault();
+        console.log(proteinInput)
+    }
+
+    function plusProtein(){
+        dispatch({type: "ADD"})
+    }
+
   return (
-    <div>Reducer</div>
+    <div>
+        <form onSubmit={addProtein}>
+            <textarea typeof="text" value={proteinInput} onChange={(e) => setInput(e.target.value)}/>
+            <button type='submit'>Add Protein</button>
+        </form>
+        {state.protein}
+        <button onClick={plusProtein}>+</button>
+    </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import Reducer from './components/reducer/Reducer'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <Reducer />
   </StrictMode>,
 )
