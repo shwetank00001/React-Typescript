c734b6856ecd91ec132cbd2942a97302be52192d
Shwetank <mshwetank@gmail.com>
2025-09-05 10:08:36 +0530

    Done with reducer in typescript

--- src/components/reducer/Reducer.tsx
+++ src/components/reducer/Reducer.tsx
@@ -1,15 +1,28 @@
 import { useReducer, useState } from 'react'
 
 const Reducer = () => {
-
-    function reducer(state, action){
+    function reducer(state:any, action: any){
+        if(action.type === "ADD"){
+            const newValue = [...state.protein, action.payload]
+            return {
+                protein : newValue
+            }
+        }
+        if(action.type === "DEL"){
+            const newValue = state.protein.filter(function(item:any){
+                return item.id !== action.payload
+            })
+            return {
+                protein: newValue
+            }
+        }
     }
 
     const defaultState = {
-        protein: 0
+        protein: []
     }
 
-    const [state, dispatch] = useReducer(Reducer, defaultState);
+    const [state, dispatch] = useReducer(reducer, defaultState);
     const [proteinInput, setInput] = useState('');
 
     const addProtein = (e:React.FormEvent<HTMLFormElement>) => {
@@ -18,17 +31,37 @@ const Reducer = () => {
     }
 
     function plusProtein(){
-        dispatch({type: "ADD"})
+        const newProteinData = {
+            id: new Date().getTime(),
+            proteinItem: proteinInput
+        }
+        dispatch({type: "ADD", payload: newProteinData})
     }
+
+    const removeItem =(idParam:number) =>{
+        dispatch({type:"DEL", payload: idParam})
+    } 
+
+    const ele = state?.protein.map((item:any) => {
+        console.log(typeof item)
+        return (
+            <div key={item.id}>
+                <h3>{item.proteinItem}</h3>
+                <button onClick={() => {removeItem(item.id)}}>Delete</button>
+            </div>
+        )
+    })
 
   return (
     <div>
         <form onSubmit={addProtein}>
             <textarea typeof="text" value={proteinInput} onChange={(e) => setInput(e.target.value)}/>
-            <button type='submit'>Add Protein</button>
         </form>
-        {state.protein}
+
         <button onClick={plusProtein}>+</button>
+        <>
+            {ele}
+        </>
     </div>
   )
 }
